2e457a2f2eaca30065971d7c55a263583dc9523a
arirismansyah <16.9023@stis.ac.id>
2021-10-08 16:46:29 +0700

    update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from genericpath import isdir
 import smtplib
 import ssl
 
@@ -11,27 +12,53 @@ import os
 import pandas as pd
 import numpy as np
 import xlsxwriter
-import mysql.connector
 import base64
 import json
-from datetime import time
+from datetime import datetime, time
 import json
 
 from flask import Flask, app, redirect, url_for, stream_with_context, render_template, Response, jsonify, request, make_response, send_file, send_from_directory, session
 from flask_ngrok import run_with_ngrok
-import flask_mysqldb
 import flask_migrate
 from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_mysqldb import MySQL
 
 from email_template import EmailTemplate
 
+from config import Config
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 app = Flask(__name__)
-app.secret_key = 'klik-jempol'
+app.config.from_object(Config)
+mysql = MySQL(app)
+
 
 @app.route('/')
 def landing():
     return render_template('home.html', title='Landing')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    pass
+
+@app.route('/upload_customers', methods=['GET', 'POST'])
+def upload_customers():
+    if request.method == 'POST' :
+        folder_target = os.path.join(APP_ROOT, 'static', 'upload_customers')
+        if (not os.path.isdir(folder_target)):
+            os.mkdir(folder_target)
+        
+        # get file
+        file_customer = request.files.get('file_customers')
+        
+        if(file_customer.filename.endswith('.xlsx')):
+            file_name = datetime.strftime(datetime.now(), '%Y%m%d%H%M%S')+'_data_customers.xlsx'
+            file_customer.save(os.path.join(folder_target, file_name))
+
+            df_customers = pd.read_excel(os.path.join(folder_target, file_name))
+        return make_response('success', 200)
+
 
 @app.route('/tools')
 def home():
---
+++ config.py
@@ -0,0 +1,6 @@
+class Config(object):
+    SECRET_KEY = 'klik_jempol'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = ''
+    MYSQL_DB = 'klik_jempol'
